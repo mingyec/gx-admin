f5c1431af16376a8ee3961cc2fd1af3ab62ed351
mingyec <mingyec@163.com>
2017-08-08 12:38:21 +0800

    修改标题

--- client/components/CompanyManager.vue
+++ client/components/CompanyManager.vue
@@ -4,11 +4,39 @@
     }
 </style>
 <template>
-    <Form class="layout" ref="company" :label-width="80" :model="newAccountForm">
-        <Form-item label="请选择客户">
-            <Select v-model="newAccountForm.companyID" placeholder="请选择所属客户">
-                <Option style="float:left;" v-for="key in companyItems" :value="key.companyID" :key="key.companyID">{{key.companyName}}</Option>
-            </Select>
+    <Form class="layout" ref="company" :label-width="100" :model="Companys">
+        <Row>
+            <Col span="8">
+                <Form-item label="请选择客户">
+                    <Select v-model="Companys.companyID" placeholder="请选择所属客户">
+                        <Option style="float:left;" v-for="key in companyItems" :value="key.companyID" :key="key.companyID">{{key.companyName}}</Option>
+                    </Select>
+                </Form-item>
+            </Col>
+            <Col span="2">
+                <Button type="primary">新增客户</Button>
+            </Col>
+        </Row>
+        <Form-item label="全厂容量">
+            <Input placeholder="请填写"></Input>
+        </Form-item>
+        <Form-item label="地址">
+            <Input placeholder="请填写"></Input>
+        </Form-item>
+        <Form-item label="经度">
+            <Input placeholder="请填写"></Input>
+        </Form-item>
+        <Form-item label="纬度">
+            <Input placeholder="请填写"></Input>
+        </Form-item>
+        <Form-item label="缓冲单元mac">
+            <Input placeholder="请填写"></Input>
+        </Form-item>
+        <Form-item label="用电分类">
+            <Input placeholder="请填写"></Input>
+        </Form-item>
+        <Form-item label="服务方式">
+            <Input placeholder="请填写"></Input>
         </Form-item>
     </Form>
 </template>
@@ -18,7 +46,7 @@
     export default {
         data () {
             return {
-                newAccountForm: {
+                Companys: {
 
                 },
                 companyItems: []
